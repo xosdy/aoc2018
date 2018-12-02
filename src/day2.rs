ce147afbe0d094938c60630eaa6146182da3d7c0
Tang King Fai <xosdy.t@gmail.com>
2018-12-02 20:05:25 +0800

    Add day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -26,6 +26,30 @@ pub fn solve_part1(input: &str) -> u32 {
     twos * threes
 }
 
+#[aoc(day2, part2)]
+pub fn solve_part2(input: &str) -> String {
+    for (line_index, line1) in input.lines().enumerate() {
+        for line2 in input.lines().skip(line_index + 1) {
+            let mut diff_count = 0;
+            let mut last_diff_index = 0;
+            for (i, (c1, c2)) in line1.chars().zip(line2.chars()).enumerate() {
+                if c1 != c2 {
+                    diff_count += 1;
+                    last_diff_index = i;
+                }
+            }
+
+            if diff_count == 1 {
+                let mut id = line1.to_owned();
+                id.remove(last_diff_index);
+                return id;
+            }
+        }
+    }
+
+    unreachable!();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,6 +67,22 @@ abcdee
 ababab"
             ),
             12
+        );
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            solve_part2(
+                r"abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz"
+            ),
+            "fgij"
         );
     }
 }
