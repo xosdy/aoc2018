a0fa55c645ebc39d520741eb7098fd588b8bc5dc
Tang King Fai <xosdy.t@gmail.com>
2018-12-05 02:13:34 +0800

    Add day4 part2

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,4 @@
-use chrono::{Duration, NaiveDate, NaiveDateTime, Timelike};
+use chrono::{NaiveDateTime, Timelike};
 use std::collections::HashMap;
 use std::ops::Range;
 
@@ -31,11 +31,6 @@ pub fn input_generator(input: &str) -> Vec<Record> {
         .for_each(|(date_time, raw_record)| {
             if raw_record.ends_with("begins shift") {
                 let id = raw_record.split(' ').nth(1).unwrap()[1..].parse().unwrap();
-                let date = if date_time.hour() != 0 {
-                    date_time.date() + Duration::days(1)
-                } else {
-                    date_time.date()
-                };
                 records.push(Record {
                     id,
                     time_intervals: Vec::new(),
@@ -60,9 +55,9 @@ pub fn input_generator(input: &str) -> Vec<Record> {
     records
 }
 
-#[aoc(day4, part1)]
-pub fn solve_part1(records: &Vec<Record>) -> u32 {
+pub fn get_sleep_times_by_guard(records: &Vec<Record>) -> HashMap<u32, [u32; 60]> {
     let mut sleep_times_by_guard = HashMap::<u32, [u32; 60]>::new();
+
     for record in records {
         sleep_times_by_guard
             .entry(record.id)
@@ -84,6 +79,12 @@ pub fn solve_part1(records: &Vec<Record>) -> u32 {
             });
     }
 
+    sleep_times_by_guard
+}
+
+#[aoc(day4, part1)]
+pub fn solve_part1(records: &Vec<Record>) -> u32 {
+    let sleep_times_by_guard = get_sleep_times_by_guard(records);
     let guard_with_max_minutes = sleep_times_by_guard
         .iter()
         .max_by_key::<u32, _>(|(_, times)| times.iter().sum())
@@ -97,6 +98,20 @@ pub fn solve_part1(records: &Vec<Record>) -> u32 {
         .0 as u32;
 
     guard_with_max_minutes * sleep_in_most_minute
+}
+
+#[aoc(day4, part2)]
+pub fn solve_part2(records: &Vec<Record>) -> u32 {
+    let sleep_times_by_guard = get_sleep_times_by_guard(records);
+    let (guard, times, _) = sleep_times_by_guard
+        .iter()
+        .map(|(guard, times)| {
+            let minute_and_times = times.iter().enumerate().max_by_key(|x| x.1).unwrap();
+            (guard, minute_and_times.0 as u32, minute_and_times.1)
+        })
+        .max_by_key(|x| x.2)
+        .unwrap();
+    guard * times
 }
 
 #[cfg(test)]
@@ -126,6 +141,32 @@ mod tests {
 [1518-11-05 00:55] wakes up"
             )),
             240
+        );
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(
+            solve_part2(&input_generator(
+                r"[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up"
+            )),
+            4455
         );
     }
 }
