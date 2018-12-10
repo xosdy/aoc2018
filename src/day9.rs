1e9d40ac5d259fa9552ffa6b7fb3503cf89c7154
Tang King Fai <xosdy.t@gmail.com>
2018-12-10 17:21:49 +0800

    Add day9 part2

--- src/day9.rs
+++ src/day9.rs
@@ -1,3 +1,27 @@
+use std::collections::VecDeque;
+
+trait Rotatable {
+    fn rotate_left(&mut self, count: usize);
+    fn rotate_right(&mut self, count: usize);
+}
+
+impl<T> Rotatable for VecDeque<T> {
+    fn rotate_left(&mut self, count: usize) {
+        for _ in 0..count {
+            let tmp = self.pop_back().unwrap();
+            self.push_front(tmp);
+        }
+    }
+
+    fn rotate_right(&mut self, count: usize) {
+        for _ in 0..count {
+            let tmp = self.pop_front().unwrap();
+            self.push_back(tmp);
+        }
+    }
+}
+
+#[derive(Clone)]
 pub struct Game {
     players: usize,
     last_marble: u32,
@@ -7,24 +31,15 @@ impl Game {
     pub fn run(&self) -> u32 {
         let mut scores: Vec<u32> = vec![0; self.players];
 
-        let mut current_marble_index: isize = 0;
-        let mut marbles = vec![0];
+        let mut marbles = VecDeque::new();
+        marbles.push_back(0);
         for (marble, player_index) in (1..=self.last_marble).zip((0..scores.len()).cycle()) {
             if marble % 23 != 0 {
-                current_marble_index += 2;
-                if current_marble_index > marbles.len() as isize {
-                    current_marble_index -= marbles.len() as isize;
-                }
-
-                marbles.insert(current_marble_index as usize, marble);
+                marbles.rotate_left(2);
+                marbles.push_back(marble);
             } else {
-                current_marble_index -= 7;
-                if current_marble_index < 0 {
-                    current_marble_index += marbles.len() as isize;
-                }
-
-                scores[player_index] += marble;
-                scores[player_index] += marbles.remove(current_marble_index as usize);
+                marbles.rotate_right(7);
+                scores[player_index] += marble + marbles.pop_back().unwrap();
             }
         }
 
@@ -43,6 +58,13 @@ pub fn input_generator(input: &str) -> Box<Game> {
 
 #[aoc(day9, part1)]
 pub fn solve_part1(game: &Game) -> u32 {
+    game.run()
+}
+
+#[aoc(day9, part2)]
+pub fn solve_part2(game: &Game) -> u32 {
+    let mut game = game.clone();
+    game.last_marble *= 100;
     game.run()
 }
 
