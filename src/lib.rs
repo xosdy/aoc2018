d951e65b886eb8e601c58500a3b865338ca1c97c
Tang King Fai <xosdy.t@gmail.com>
2018-12-21 15:42:49 +0800

    Refactor vm code

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,8 @@ extern crate strum;
 #[macro_use]
 extern crate strum_macros;
 
+pub mod vm;
+
 pub mod day1;
 pub mod day10;
 pub mod day11;
@@ -25,6 +27,7 @@ pub mod day15;
 pub mod day16;
 pub mod day17;
 pub mod day18;
+pub mod day19;
 pub mod day2;
 pub mod day3;
 pub mod day4;
