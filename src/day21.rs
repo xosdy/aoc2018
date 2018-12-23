dd99fcdbce87deeaff7a78327594ff730ce825e5
Tang King Fai <xosdy.t@gmail.com>
2018-12-23 20:09:16 +0800

    Add day21 part2

--- src/day21.rs
+++ src/day21.rs
@@ -1,4 +1,5 @@
 use crate::vm::*;
+use std::collections::HashSet;
 
 #[aoc(day21, part1)]
 pub fn solve_part1(input: &str) -> usize {
@@ -8,4 +9,25 @@ pub fn solve_part1(input: &str) -> usize {
     }
 
     vm.registers.0[vm.program[28].input1]
+}
+
+#[aoc(day21, part2)]
+pub fn solve_part2(input: &str) -> usize {
+    let mut vm: Vm = input.parse().unwrap();
+    let mut numbers = HashSet::new();
+    let mut last = 0;
+
+    loop {
+        vm.step();
+
+        if *vm.ip() == 28 {
+            if numbers.insert(vm.registers.0[vm.program[28].input1]) {
+                last = vm.registers.0[vm.program[28].input1];
+            } else {
+                break;
+            }
+        }
+    }
+
+    last
 }
