683d61495ad415ea49dba188b1df5b4045554854
Tang King Fai <xosdy.t@gmail.com>
2018-12-27 21:12:56 +0800

    Add day23 part2

--- src/day23.rs
+++ src/day23.rs
@@ -1,6 +1,7 @@
 use na::Vector3;
 use regex::Regex;
-use std::collections::HashMap;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 
 pub struct Nanobot {
     position: Vector3<i64>,
@@ -38,11 +39,39 @@ pub fn solve_part1(bots: &[Nanobot]) -> usize {
         .count()
 }
 
+#[aoc(day23, part2)]
+pub fn solve_part2(bots: &[Nanobot]) -> u64 {
+    let mut pq = BinaryHeap::new();
+    for bot in bots {
+        let dist = distance(bot.position, Vector3::zeros());
+        pq.push((Reverse(dist.saturating_sub(bot.radius)), 1));
+        pq.push((Reverse(dist + bot.radius), -1));
+    }
+
+    let mut count = 0;
+    let mut max_count = 0;
+    let mut min_dist = 0;
+    while let Some((dist, diff)) = pq.pop() {
+        count += diff;
+
+        if count > max_count {
+            max_count = count;
+            min_dist = dist.0;
+        }
+    }
+
+    min_dist
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = r"pos=<0,0,0>, r=4
+    #[test]
+    fn part1() {
+        assert_eq!(
+            solve_part1(&input_generator(
+                r"pos=<0,0,0>, r=4
 pos=<1,0,0>, r=1
 pos=<4,0,0>, r=3
 pos=<0,2,0>, r=1
@@ -50,10 +79,24 @@ pos=<0,5,0>, r=3
 pos=<0,0,3>, r=1
 pos=<1,1,1>, r=1
 pos=<1,1,2>, r=1
-pos=<1,3,1>, r=1";
+pos=<1,3,1>, r=1"
+            )),
+            7
+        );
+    }
 
     #[test]
-    fn part1() {
-        assert_eq!(solve_part1(&input_generator(TEST_INPUT)), 7);
+    fn part2() {
+        assert_eq!(
+            solve_part2(&input_generator(
+                r"pos=<10,12,12>, r=2
+pos=<12,14,12>, r=2
+pos=<16,12,12>, r=4
+pos=<14,14,14>, r=6
+pos=<50,50,50>, r=200
+pos=<10,10,10>, r=5"
+            )),
+            36
+        )
     }
 }
